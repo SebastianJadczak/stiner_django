1ad908756e690c4a3e84344c5d5e7b7306fd13ee
Sebastian <s.jadczak@il-pib.pl>
2020-04-16 16:28:10 +0200

    add mixin

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,10 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.PostsListView.as_view(), name='list_posts')
+    path('', views.PostsListView.as_view(), name='list_posts'),
+    path('manage/',views.ManagePostListView.as_view() , name='manage_post_list'),
+    path('create/',views.PostCreateView.as_view() , name='post_create'),
+    path('<pk>/edit/',views.PostUpdateView.as_view() , name='post_edit'),
+    path('<pk>/delete/',views.PostDeleteView.as_view() , name='post_delete'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,50 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
+
+class OwnerMixin(object):
+    def get_queryset(self):
+        qs = super(OwnerMixin, self).get_queryset()
+        return qs.filter(owner=self.request.user)
+
+class OwnerEditMixin(object):
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super(OwnerEditMixin, self).form_valid(form)
+
+class OwnerPostMixin(OwnerMixin, LoginRequiredMixin):
+    model = Post
+    fields = ['title', 'image', 'body', 'slug']
+    success_url = reverse_lazy('manage_posts_list')
+
+class OwnerPostEditMixin(OwnerPostMixin, OwnerEditMixin):
+    fields =['title', 'image', 'body', 'slug']
+    success_url = reverse_lazy('manage_posts_list')
+    template_name = 'blog/manage/posts/form.html'
+
+class ManagePostListView(ListView):
+    """ Widok wyświetlający wszystkie posty stworzone przez konkretnego użytkownika """
+    model = Post
+    template_name = 'blog/manage/posts/list.html'
+
+class PostCreateView(PermissionRequiredMixin ,OwnerPostEditMixin, CreateView):
+   permission_required = 'posts.add_post'
+
+class PostUpdateView(PermissionRequiredMixin ,OwnerPostEditMixin, UpdateView):
+    template_name = 'blog/manage/posts/form.html'
+    permission_required = 'posts.change_post'
+
+class PostDeleteView(OwnerPostMixin, DeleteView):
+    template_name = 'blog/manage/posts/delete.html'
+    success_url = reverse_lazy('manage_posts_list')
+    permission_required = 'posts.delete_post'
+
+
 class PostsListView(ListView):
+    """ Widok wyświetlający wszystkie posty na blogu """
     model = Post
     template_name = 'blog/list.html'
 
