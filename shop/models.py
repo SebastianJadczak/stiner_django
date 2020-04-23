88106ab60127f20bef58413cae659ecc2b0d3b78
Sebastian <s.jadczak@il-pib.pl>
2020-04-23 16:39:26 +0200

    add example photos of categories for test

--- shop/models.py
+++ shop/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
+    image = models.ImageField(upload_to='media/category/%Y/%m/%d', blank=True)
 
     class Meta:
         ordering = ('name',)
