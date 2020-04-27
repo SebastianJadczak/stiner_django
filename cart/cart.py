68b0d992887b20ecb6e941af41315bf9e98cc463
Sebastian <s.jadczak@il-pib.pl>
2020-04-27 12:24:54 +0200

    add method add product to cart/update amount product

--- cart/cart.py
+++ cart/cart.py
@@ -11,3 +11,20 @@ class Cart(object):
         if not cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+    def add(self, product, quantity=1, update_quantity=False):
+        """ Dodawanie produktu do koszyka, bądź zmiana jego ilości w koszyku """
+
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity':0, 'price': str(product.price)}
+
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        """ Oznacznie modyfikacji sesji """
+        self.session.modified = True
