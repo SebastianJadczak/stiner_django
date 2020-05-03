45be62c9004422760b5d9cf562ecee7c44abff37
Sebastian <s.jadczak@il-pib.pl>
2020-05-03 19:18:30 +0200

    add to first point in Warsaw (church Anny)

--- map/map.py
+++ map/map.py
@@ -7,14 +7,10 @@ m = folium.Map(location=[52.2297700, 21.0117800], zoom_start=12)
 tooltip = 'Kliknij po więcej informacji'
 
 # Create markers
-folium.Marker([42.3601, -71.0589] ,
-              popup='<strong> Pierwszy punkt na mapie </strong> <a href="/management/blog_/">Więcej</a>',
+folium.Marker([52.2460643, 21.0140727] ,
+              popup='<strong> Kościół św. Anny w Warszawie – świątynia rzymskokatolicka znajdująca się przy Krakowskim Przedmieściu 68 w dzielnicy Śródmieście, główny ośrodek duszpasterstwa akademickiego w Warszawie. Wikipedia</strong> ',
               tooltip=tooltip).add_to(m)
 
-folium.Marker([42.36201, -71.1589],
-              popup='<strong> Pierwszy punkt na mapie </strong>',
-              tooltip=tooltip,
-              icon=folium.Icon(icon='cloud')).add_to(m)
 
 # Generate map
 m.save('templates/map/map.html')
