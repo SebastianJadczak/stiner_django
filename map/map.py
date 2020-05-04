6348f4408514d7355780a1cb6dc02f0eb837c42d
Sebastian <s.jadczak@il-pib.pl>
2020-05-04 15:26:47 +0200

    add div and styles to trails views

--- map/map.py
+++ map/map.py
@@ -4,13 +4,13 @@ import folium
 
 m = folium.Map(location=[52.2297700, 21.0117800], zoom_start=12)
 # Global tooltip
+
 tooltip = 'Kliknij po więcej informacji'
 
 # Create markers
 folium.Marker([52.2460643, 21.0140727] ,
               popup='<strong> Kościół św. Anny w Warszawie – świątynia rzymskokatolicka znajdująca się przy Krakowskim Przedmieściu 68 w dzielnicy Śródmieście, główny ośrodek duszpasterstwa akademickiego w Warszawie. Wikipedia</strong> ',
               tooltip=tooltip).add_to(m)
-
 
 # Generate map
 m.save('templates/map/map.html')
--- trails/views.py
+++ trails/views.py
@@ -3,7 +3,7 @@ from django.views import View
 
 
 class Trails(View):
-    template_name= 'trails/basic_trails.html'
+    template_name= 'trails/trails.html'
 
     def get(self, request):
         return render(request, self.template_name)
