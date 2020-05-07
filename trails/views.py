9d146bf429ac072ebf8aaf962032b02f4dd103cf
Sebastian <s.jadczak@il-pib.pl>
2020-05-07 16:34:19 +0200

    fix bug with search points

--- trails/views.py
+++ trails/views.py
@@ -14,7 +14,7 @@ class PointsListView(ListView):
         template_name = 'points/points.html'
         query = request.GET.get('search')
         if query:
-            self.list = self.list.filter(name=query)
+            self.list = self.list.filter(name__contains=query)
         return render(request, template_name,
                       {'list': self.list})
 
