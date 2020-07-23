b275ca4a16db950e11874bee9049585914fdec8a
Sebastian <s.jadczak@il-pib.pl>
2020-07-23 15:42:54 +0200

    poprawa wyglądu listy ciekawych miejsc + dodanie danych testowych

--- map/models.py
+++ map/models.py
@@ -11,8 +11,8 @@ class Point(models.Model):
     )
 
     name = models.CharField(max_length=30)
-    coordinateX = models.DecimalField(max_length=30, decimal_places=5, max_digits=100)
-    coordinateY = models.DecimalField(max_length=30, decimal_places=5, max_digits=100)
+    coordinateX = models.DecimalField(max_length=30, decimal_places=7, max_digits=100)
+    coordinateY = models.DecimalField(max_length=30, decimal_places=7, max_digits=100)
     image = models.ImageField(upload_to='media/img_point/%Y/%m%d')
     descriptions = models.TextField()
     type = models.CharField(max_length=10, choices=TYPE_POINT)
