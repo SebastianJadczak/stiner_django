fa9875c5bc66580043f36ee305b49b0c5926c071
Sebastian <s.jadczak@il-pib.pl>
2020-07-28 14:55:21 +0200

    dodanie przycisków w sekcji Tworzenia tras, obsługa tablicy itp.

--- user_trails/views.py
+++ user_trails/views.py
@@ -1,7 +1,12 @@
+from urllib import request
+
 from django.conf import settings
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView, TemplateView, DetailView
 
+from cart.forms import CartAddProductForm
 from map.models import Point
 from trails.models import Trail
 
@@ -11,23 +16,28 @@ class UserTrailsListView(ListView):
     model = Trail
 
 
-class UserTrailFormAdd(TemplateView):
+class UserTrailFormAdd(ListView):
     template_name ='trails/user_trails/form_trails.html'
+    model = Point
+    list = Point.objects.all()
+    user = []
+
+    def get(self, request, *args, **kwargs):
+        query = request.GET.get('add_point')
+        if query:
+            self.user.append(self.list.filter(name=query))
+            return redirect('user_trails:user_trail_add')
+        return render(request, self.template_name,
+                      {'list': self.list, 'user':self.user})
 
 
-    def get_context_data(self, **kwargs):
-        context = super(UserTrailFormAdd, self).get_context_data(**kwargs)
-        context['lists'] = Point.objects.all()
+class DetailPoint(DetailView):
+    template_name = 'trails/user_trails/detail.html'
+    model = Point
+
+    def get_context_data(self,*args, **kwargs):
+        context = super().get_context_data(**kwargs)
         return context
 
-class AddPointToTrail(object):
 
-    def __init__(self, request):
-        """ inicjalizacja tworzenia tras """
-
-        self.session = request.session
-        trail = self.session.get(settings.AddPointToTrail_SESSION_ID)
-        if not trail:
-            trail = self.session[settings.AddPointToTrail_SESSION_ID] = {}
-        self.trail = trail
 
