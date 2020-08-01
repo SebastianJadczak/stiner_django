4d36d4274c1ce195f33e447947e16bedc64ea092
Sebastian <s.jadczak@il-pib.pl>
2020-08-01 23:22:46 +0200

    dodanie, usuniętych punktów + dodanie blokowania dodanych wcześniej punktów

--- user_trails/views.py
+++ user_trails/views.py
@@ -1,3 +1,4 @@
+import json
 from urllib import request
 
 from django.conf import settings
@@ -23,16 +24,28 @@ class UserTrailFormAdd(ListView):
     model = Point
     list = Point.objects.all()
     user = []
+    zmienna = ""
+
+
+    def z(self, query):
+        for a in self.user:
+            for z in a:
+                if query == str(z):
+                    return False
 
     def get(self, request, *args, **kwargs):
+        request.GET._mutable = True
         query = request.GET.get('add_point')
         if query:
-            self.user.append(self.list.filter(name=query))
-
-            return redirect('user_trails:user_trail_add')
+            z = self.z(query)
+            if z == False:
+                self.zmienna+="Nie możesz dodać tego zabytku. Istnieje już w bazie"
+            else:
+                self.user.append(self.list.filter(name=query))
         len_user = len(self.user)
+
         return render(request, self.template_name,
-                      {'list': self.list, 'user':self.user, 'len_user':len_user})
+                      {'list': self.list, 'user':self.user, 'len_user':len_user, 'zmienna':self.zmienna })
 
 class UserTrailDraft(UserTrailFormAdd):
     """Klasa odpowiedzialna za Szkic trasy zwiedzania"""
