4608218b3af76c5faeb979023a4376eb7caa09dc
Sebastian <s.jadczak@il-pib.pl>
2020-08-22 19:14:59 +0200

    zrobienie strony rejestracji + poprawki

--- map/forms.py
+++ map/forms.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class FormularzRejestracji(forms.ModelForm):
     username = forms.CharField(label="Login:", max_length=30, widget=forms.TextInput(attrs={'class': 'username_register first', }))
-    email = forms.EmailField(label="Email: ",widget=forms.TextInput(attrs={'class': 'username_register'}))
-    password = forms.CharField(label='Hasło: ', widget=forms.PasswordInput(attrs={'class': 'username_register'}))
+    email = forms.EmailField(label="Email:",widget=forms.TextInput(attrs={'class': 'username_register'}))
+    password = forms.CharField(label='Hasło:', widget=forms.PasswordInput(attrs={'class': 'username_register'}))
 
     #Klasa dzięki czemu użytkownikom nie są dodawane 's' na końcu
     class Meta:
