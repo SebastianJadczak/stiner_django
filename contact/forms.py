0286739b6c69a602a163413da309e775dc9b5f9a
Sebastian <s.jadczak@il-pib.pl>
2020-08-23 15:39:02 +0200

    strona kontakt zrobiona + style + formularz

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,6 @@ from django import forms
 class EmailForm(forms.Form):
     """ Formularz wysyłania wiadomości email """
 
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    comments = forms.CharField(required=False, widget=forms.Textarea)
+    name = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'placeholder': 'Podaj swoje imię', 'class' : 'name'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Podaj swój adres email', 'class' : 'email'}))
+    comments = forms.CharField(required=False, widget=forms.Textarea(attrs={'placeholder': 'Tu wpisz wiadomość do Nas.', 'class' : 'text'}))
