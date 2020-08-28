ec11c628fe323c68fd7beb30e5fdbcf90c7fb56d
Sebastian <s.jadczak@il-pib.pl>
2020-08-28 17:41:44 +0200

    wypełnianie sekcji użytkownika

--- account/models.py
+++ account/models.py
@@ -5,6 +5,8 @@ from django.conf import settings
 class Profile(models.Model):
     """Nadpisanie modelu User."""
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.TextField(max_length=30)
+    surname = models.TextField(max_length=30)
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
     country = models.TextField(max_length=30, blank=True)
@@ -15,6 +17,7 @@ class Profile(models.Model):
     postal_code = models.CharField(max_length=20, blank=True)
     main_language = models.CharField(max_length=25, blank=True)
     other_language = models.CharField(max_length=20, blank=True)
+    phone = models.TextField(max_length=10)
 
     def __str__(self):
         return 'Profil użytkownika {}.'.format(self.user.username)
--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -7,3 +10,5 @@ urlpatterns = [
     path('my_account', views.UserAccount.as_view(), name='user_account'),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
