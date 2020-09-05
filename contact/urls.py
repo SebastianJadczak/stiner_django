734c61b0747b7c99d26ca8c3aa7f457e5d9f22fc
Sebastian <s.jadczak@il-pib.pl>
2020-09-05 16:02:33 +0200

    naprawa błędu z nazwą aplikacji kontakt + dodanie reklamy na strone bloga

--- contact/urls.py
+++ contact/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 from .import views
 
-app_name = 'management'
+app_name = 'contact'
 
 urlpatterns = [
     path('',views.post_share , name='contact_detail'),
