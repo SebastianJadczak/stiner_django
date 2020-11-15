18c544ed05caf4b46d2fa4cdd3a711d2a40a5dcc
sjadczak <sebastian-jadczak@wp.pl>
2020-11-15 02:21:46 +0100

    fix bug with form add opinion to the point

--- trails/views.py
+++ trails/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, TemplateView, DetailView
-from rest_framework import  generics
+from rest_framework import generics
 
 from map.models import Point, Opinion_about_Point
 from shop.models import Category
@@ -17,6 +17,7 @@ class Trails(TemplateView):
         context['category'] = Category.objects.all()
         return context
 
+
 class PointsListView(ListView):
     """ Widok odpowiedzialny za listę punktów + wyszukiwarka """
     template_name = 'points/points.html'
@@ -28,10 +29,9 @@ class PointsListView(ListView):
         query_location = request.GET.get('location_points')
         query_type = request.GET.get('point_type')
         if query_name or query_location or query_type:
-            self.list = self.list.filter(name__contains=query_name, type=query_type, location__contains= query_location)
+            self.list = self.list.filter(name__contains=query_name, type=query_type, location__contains=query_location)
         return render(request, template_name,
                       {'list': self.list})
-
 
 
 class PointDetailView(DetailView):
@@ -40,26 +40,30 @@ class PointDetailView(DetailView):
     template_name = 'points/point/point_detail.html'
     model = Point
 
-    def get(self, request, *args, **kwargs):
-        nick = request.GET.get('nick')
-        star = request.GET.get('star')
-        recension = request.GET.get('recension')
-        object = list(Point.objects.filter(id=self.kwargs['pk']))
+    def post(self, request, *args, **kwargs):
+        nick = request.POST.get('nick')
+        star = request.POST.get('star')
+        point_id = request.POST.get('point_id')
+        recension = request.POST.get('recension')
         opinion = list(Opinion_about_Point.objects.filter(point=self.kwargs['pk']))
         if nick and star and recension:
             Opinion_about_Point.objects.create(user=nick,
                                                opinion=recension,
                                                rating=star,
-                                               point=object[0])
+                                               point=Point.objects.get(id=point_id))
         else:
-            if nick== "":
+            if nick == "":
                 print('brak nicku')
-            elif star== None:
+            elif star == None:
                 print('Brak gwiazdy')
-            elif recension== "" :
+            elif recension == "":
                 print('Brak recenzji')
+        return redirect('../../point/' + point_id)
 
-        return render(request, self.template_name, {'object':object, 'opinion':opinion})
+    def get(self, request, *args, **kwargs):
+        opinion = list(Opinion_about_Point.objects.filter(point=self.kwargs['pk']))
+        point_id = self.kwargs['pk']
+        return render(request, self.template_name, {'opinion': opinion, 'point_id': point_id})
 
 
 class TrailsListView(ListView):
@@ -72,16 +76,16 @@ class TrailsListView(ListView):
         return trails
 
 
-
 class TrailDetailView(DetailView):
     """ Widok odpowiedzialny za wyświetlenie szczegółowych informacji wybranego miejsca """
 
     template_name = 'points/point/point_detail.html'
     model = Trail
 
-    def get_context_data(self,*args, **kwargs):
+    def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
 
 class TrailApiFilterListView(generics.ListAPIView):
     """ Widok odpowiedzialny za filtrowanie obiektów w zależności od trasy którą użytkownik wybierze :) """
@@ -90,4 +94,3 @@ class TrailApiFilterListView(generics.ListAPIView):
     def get_queryset(self):
         pk = self.kwargs['pk']
         return Point.objects.filter(trails=pk)
-
