f93cb72ade20b3f364ecbed32b22a48f8847972d
sjadczak <sebastian-jadczak@wp.pl>
2020-12-13 20:37:51 +0100

    add test to model Preference

--- map/test/tests_models_account.py
+++ map/test/tests_models_account.py
@@ -10,10 +10,10 @@ class PostModelTestCase(TestCase):
     def setUp(self):
         """Zdefiniujemy obiekty na podstawie modelu."""
         self.user = User.objects.create_user(username='admin', password='Test123')
-        self.profile = Profile.objects.create(user=self.user, name='Sebastian', surname='Jadczak', date_of_birth='1990-6-21', photo=None, country='Polska',
-                                              city='Warszawa', street='Testowa', house_number=12, apartment_number=1, postal_code='01-111', main_language='Polish',
-                                              other_language="English", phone='123456789', email='test@test.pl')
-
+        self.profile = Profile.objects.create(user=self.user, name='Sebastian', surname='Jadczak', date_of_birth='1990-6-21', photo=None, country='Polska',  city='Warszawa', street='Testowa', house_number=12,
+                                              apartment_number=1, postal_code='01-111', main_language='Polish',  other_language="English", phone='123456789', email='test@test.pl')
+        self.preference = Preference.objects.create(user=self.user, favorite_country='Polska', favorite_region='Mazury',
+                                                    favorite_city='Kraków', favorite_place='Molo w Sopocie', favorite_restaurant='Dolce Vita')
 
     def test_set_Profile(self):
         """Test sprawdzenia czy obiekt został stworzony."""
@@ -23,6 +23,6 @@ class PostModelTestCase(TestCase):
         """Test sprawdzający poprawność metody zwracającą imię i nazwisko użytkownika."""
         self.assertEqual(self.profile.full_name(), 'Sebastian Jadczak')
 
-    # def test_set_Preference(self):
-    #     """Test sprawdzenia czy obiekt został stworzony."""
-    #     self.assertIsNotNone()
+    def test_set_Preference(self):
+        """Test sprawdzenia czy obiekt został stworzony."""
+        self.assertIsNotNone(self.preference.id)
