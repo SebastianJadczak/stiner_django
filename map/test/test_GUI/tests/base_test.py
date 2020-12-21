73c8f67e86137792bc82cafa321bf8523783ceeb
sjadczak <sebastian-jadczak@wp.pl>
2020-12-21 15:40:06 +0100

    add base_test (test_GUI), selenium

---
+++ map/test/test_GUI/tests/base_test.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class BaseTest:
+    """Klasa bazowa inicjalizująca driver'a i zamykająca przeglądarkę po wykonaniu testu"""
+
+    adres = 'https://stiner-dev.herokuapp.com/'
+    @pytest.fixture()
+    def setup(self):
+        """Inicjalizacja przeglądarki, opóźnienie maksymalizacji okna oraz zamknięcie okna przeglądarki po zakończeniu testu"""
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.implicitly_wait(5)
+        self.driver.maximize_window()
+        yield
+        self.driver.quit()
