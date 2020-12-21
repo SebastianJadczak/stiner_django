8936f183e0f5217d4a0ee0f96099fbe0a87a8e9f
sjadczak <sebastian-jadczak@wp.pl>
2020-12-21 15:52:30 +0100

    write method to send message on the contact page (selenium)

--- map/test/test_GUI/pages/contact_page.py
+++ map/test/test_GUI/pages/contact_page.py
@@ -1,5 +1,6 @@
 import logging
 
+
 class ContactPage:
     '''Klasa odpowiedzialna za zakładkę Kontakt.'''
 
@@ -11,6 +12,10 @@ class ContactPage:
         self.comments_textarea_by_id = 'id_comments'
         self.send_input_by_xpath = "//input[@value='Wyślij']"
 
-    def send_message(self):
+    def send_message(self, name, email, comments):
         """Wypełniamy formularz wiadomoci i wysyłamy."""
         self.logger.info('Wypełnianie formularzu')
+        self.driver.find_element_by_id(self.name_input_by_id).send_keys(name)
+        self.driver.find_element_by_id(self.email_input_by_id).send_keys(email)
+        self.driver.find_element_by_id(self.comments_textarea_by_id).send_keys(comments)
+        self.driver.find_element_by_xpath(self.send_input_by_xpath).click()
