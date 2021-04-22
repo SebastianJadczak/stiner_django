dbdb6ed89f496d5830670e1761e18d64ac15777f
sjadczak <sebastian-jadczak@wp.pl>
2021-04-22 18:31:35 +0200

    collect static

---
+++ static/js/base.js
@@ -0,0 +1,17 @@
+<!--    "Przycisk rozwijania menu"-->
+console.log("base.js")
+let menu = document.querySelector("button.user_menu");
+let icon = document.querySelector("i.fa-angle-down");
+let user_menu = document.querySelector("div.user_menu");
+let yours_trails = document.querySelector("li.yours_trails");
+let tr = document.querySelector("div.tr")
+
+if (menu) {
+    menu.addEventListener('click', function () {
+        user_menu.classList.toggle("active");
+        icon.classList.toggle("active");
+    });
+}
+if (yours_trails) {
+    tr.classList.toggle("active");
+}
