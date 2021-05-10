db72bd4351cc32f6433a963131b09ea956f10579
SebastianJadczak <51311717+SebastianJadczak@users.noreply.github.com>
2021-05-10 15:42:39 +0200

    refactor and delete redundant lines of code js

--- blog/static/js/user_account.js
+++ blog/static/js/user_account.js
@@ -1,22 +1,6 @@
 let edit_info_user = document.getElementById('edit_info_user')
 let edit_Preference = document.getElementById('editPreference')
-//------------------------------------------
-//The method responsible for choosing login / registration at low screen resolution
-let login_rwd = document.getElementById('login_rwd')
-let login_mapa_menu = document.querySelector('.login_mapa')
-let menuDetail = document.getElementById('menu-detail-trail')
 
-//Top Menu
-function menuDetailTrail() {
-    menuDetail.classList.toggle('active')
-}
-
-
-if (login_rwd) {
-    login_rwd.addEventListener('click', function () {
-        login_mapa_menu.classList.toggle('mini')
-    })
-}
 editUserAccount = () => {
     edit_info_user.classList.toggle('active')
 }
