4f9fe0a37e0710f6473a13e152d720d9fdbe9f39
Krzysztof Kuta <atuk.krzysztof@gmail.com>
2021-05-23 15:58:20 +0200

    added sorting point in done

--- map/views.py
+++ map/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
 from django.views.generic import ListView, DetailView
@@ -157,12 +157,17 @@ class DoneList(ListView):
 
     @csrf_exempt
     def sort(request):
-        print(request.POST.get('type'))
-        return render(request,template_name='done/done.html',)
+        pass
 
     def get_context_data(self, **kwargs):
         self.done_trail = Trail.objects.filter(done=self.request.user)
         self.done_point = Point.objects.filter(done=self.request.user)
+        sorting = self.request.GET.get('sorting', "") #http//..../done/?sorting=rate ---  geting rate or done and adding to url
+        if sorting == "rate":
+            self.done_point = Point.objects.filter(done=self.request.user).order_by('-average_grade')
+        elif sorting == "done":
+            self.done_point = Point.objects.filter(done=self.request.user).order_by('done_count') #NOT WORKING
+                                                            # we should add some calculation in model to count 'done'
         context = super().get_context_data(**kwargs)
         context['trail_done'] = self.done_trail
         context['point_done'] = self.done_point
